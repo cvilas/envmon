f22dcfd3678cc7117ef6b69e3446d21957d1d603
Vilas Chitrakaran <cvilas@gmail.com>
2013-12-31 11:20:46 +0000

    started on arduino client

---
+++ station/station.ino
@@ -0,0 +1,15 @@
+/// station.ino
+/// Arduino control program for iota-station
+/// 
+
+#include <SPI.h>
+#include <WiFi.h>
+#include "connection_info.h"
+
+WiFiClient client;
+
+void setup()
+{}
+
+void loop()
+{}
